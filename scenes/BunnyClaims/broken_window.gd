4f12e8b2f647ca4f42c862f17e38ee89573c030b
Jin Xuan Chan <jxchan14@gmail.com>
2026-02-28 22:55:48 +0000

    added animations

---
+++ scenes/BunnyClaims/broken_window.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+func _ready() :
+	$AnimationPlayer.play("fade_in")
+	await $AnimationPlayer.animation_finished
--- scenes/bunny_house_in.gd
+++ scenes/bunny_house_in.gd
@@ -14,3 +14,17 @@ func _on_window_pressed() -> void:
 	$AnimationPlayer.play("fade_out")
 	await $AnimationPlayer.animation_finished
 	get_tree().change_scene_to_file("res://scenes/BunnyClaims/BrokenWindow.tscn")
+
+
+func _on_book_shelf_pressed() -> void:
+	$AnimationPlayer.play("fade_out")
+	await $AnimationPlayer.animation_finished
+	get_tree().change_scene_to_file("res://scenes/BunnyClaims/Infestation.tscn")
+	
+	
+
+
+func _on_table_pressed() -> void:
+	$AnimationPlayer.play("fade_out")
+	await $AnimationPlayer.animation_finished
+	get_tree().change_scene_to_file("res://scenes/BunnyClaims/MissingTV.tscn")
