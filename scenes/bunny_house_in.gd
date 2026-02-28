32068e98801d953c40f0e42ca019b33f5973d076
Jin Xuan Chan <jxchan14@gmail.com>
2026-02-28 21:17:03 +0000

    added claims folder

--- scenes/bunny_house_in.gd
+++ scenes/bunny_house_in.gd
@@ -5,6 +5,12 @@ func _ready():
 
 
 func _on_pan_pressed() -> void:
-	$AnimationPlayer.play("fade_out")
+	$AnimationPlayer.play("fade_")
 	await $AnimationPlayer.animation_finished
 	get_tree().change_scene_to_file("res://scenes/FireHazard.tscn")
+
+
+func _on_window_pressed() -> void:
+	$AnimationPlayer.play("fade_")
+	await $AnimationPlayer.animation_finished
+	get_tree().change_scene_to_file("res://scenes/BrokenWindow.tscn")
