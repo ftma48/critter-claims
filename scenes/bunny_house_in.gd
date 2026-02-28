05bffd34727e13151b0fd0d06367823e1c89bfc5
Jin Xuan Chan <jxchan14@gmail.com>
2026-02-28 21:10:25 +0000

    added claim 1

--- scenes/bunny_house_in.gd
+++ scenes/bunny_house_in.gd
@@ -3,5 +3,8 @@ extends Node2D
 func _ready():
 	pass
 
-func _on_back_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/BunnyHouse.tscn")
+
+func _on_pan_pressed() -> void:
+	$AnimationPlayer.play("fade_out")
+	await $AnimationPlayer.animation_finished
+	get_tree().change_scene_to_file("res://scenes/FireHazard.tscn")
