83c4667f200cf8b2249b8346517e5e78332480da
ftma48 <falaraji05@gmail.com>
2026-03-01 09:14:19 +0000

    h

--- autoload/game_state.gd
+++ autoload/game_state.gd
@@ -1,5 +1,17 @@
 extends Node
 
+enum PlanType {
+	ESSENTIAL,
+	PREMIER
+}
+
+enum Character {
+	BUNNY,
+	SEAGULL
+}
+
 var tv_seen = false
 var window_seen = false
 var current_webpage: WebPageData = null
+var plan_type: PlanType = PlanType.ESSENTIAL
+var current_character: Character
--- scenes/computer_screen.gd
+++ scenes/computer_screen.gd
@@ -8,15 +8,19 @@ extends Control
 @onready var chatter = $ChatterPlayer
 @onready var ringing = $Ringing
 
-var default_page = preload("res://webpages/my_insurance_essential.tres")
+var essential_page = preload("res://webpages/my_insurance_essential.tres")
+var premier_page = preload("res://webpages/my_insurance_premier.tres")
+var bunny_house = "res://scenes/BunnyHouseIn.tscn"
+var seagull_house = "res://scenes/SeagullHouseIn.tscn"
 var lines: Array = []
 var current_line = 0
 
 func _ready():
 	if webpage == null and GameState.current_webpage != null:
 		webpage = GameState.current_webpage
-
-	_load_page(default_page)
+	
+	_load_page(_get_default_page())
+	
 	webpage_node.visible = true
 	$CallButton.visible = false
 	$CanvasLayer/Exit.visible = false
@@ -28,6 +32,22 @@ func _ready():
 		chatter.play()
 	else:
 		dialogue_panel.visible = false
+
+func _get_default_page() -> WebPageData:
+	match GameState.plan_type:
+		GameState.PlanType.ESSENTIAL:
+			return essential_page
+		GameState.PlanType.PREMIER:
+			return premier_page
+	return essential_page
+
+func _get_house_scene() -> String:
+	match GameState.current_character:
+		GameState.Character.BUNNY:
+			return "res://scenes/BunnyHouseIn.tscn"
+		GameState.Character.SEAGULL:
+			return "res://scenes/SeagullHouseIn.tscn"
+	return "res://scenes/BunnyHouseIn.tscn"
 
 func _input(event):
 	if not dialogue_panel.visible:
@@ -53,7 +73,7 @@ func _on_exit_pressed() -> void:
 	var tree = get_tree()
 	$AnimationPlayer.play("fade_out")
 	await $AnimationPlayer.animation_finished
-	tree.change_scene_to_file("res://scenes/BunnyHouseIn.tscn")
+	tree.change_scene_to_file(_get_house_scene())
 
 
 func _on_call_button_pressed() -> void:
