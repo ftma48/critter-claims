1103cf6a770f61dfaa305cee7b6b90b4d091593d
ftma48 <falaraji05@gmail.com>
2026-03-02 13:18:29 +0000

    file reorganise

--- scenes/seagull_house_in.gd
+++ scenes/seagull_house_in.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 var window_scene = preload("res://scenes/window_storm.tscn")
-var sink_scene = preload("res://sink.tscn")
+var sink_scene = preload("res://scenes/sink.tscn")
 @onready var fly_music = $fly
 
 func _ready():
@@ -19,3 +19,9 @@ func _on_sink_pressed() -> void:
 	$AnimationPlayer.play("fade_out")
 	await $AnimationPlayer.animation_finished
 	get_tree().change_scene_to_packed(sink_scene)
+
+func _on_exit_pressed() -> void:
+	fly_music.stop()
+	$AnimationPlayer.play("fade_out")
+	await $AnimationPlayer.animation_finished
+	get_tree().change_scene_to_file("res://scenes/MainMenu.tscn")
